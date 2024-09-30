24201811526aacaaff9ad9a9371c2d799cd2e7c0
chvaldez10 <christianvaldez021@gmail.com>
2024-09-29 22:06:12 -0600

    upgrade cypress

--- automation/crafts-automation/cypress/e2e/extract-csv.cy.js
+++ automation/crafts-automation/cypress/e2e/extract-csv.cy.js
@@ -14,6 +14,6 @@ describe("Extract CSV", () => {
   it("Extract csv files", () => {
     cy.login(Cypress.env("username"), Cypress.env("password"));
     cy.navigateToAttendancePage();
-    cy.traverseDate(8, 2024, "August");
+    cy.traverseDate(9, 2024, "September");
   });
 });
